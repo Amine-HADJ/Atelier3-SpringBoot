c94930346ec1b895a87157de3e06953bbc253af3
Amine-HADJ <mohammed.hadj-hamdri@cpe.fr>
2024-05-22 16:28:34 +0200

    spring initializr

---
+++ Docker/applications/Room/src/test/java/com/cardgame/Room/RoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cardgame.Room;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
