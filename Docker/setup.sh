53524f0dabf8801039b3d9e0cf1270d271f94911
Amine-HADJ <mohammed.hadj-hamdri@cpe.fr>
2024-05-25 21:12:56 +0200

    fixed dockers

--- Docker/setup.sh
+++ Docker/setup.sh
@@ -2,27 +2,12 @@
 
 # This script will generate .jar for each application ('mvn clean package') and then run all the neccessary containers for the project.
 
-cd ./applications/auth
-mvn clean package
+cd ./applications/auth && mvn clean package && cd -
+cd ./applications/card && mvn clean package && cd -
+cd ./applications/game && mvn clean package && cd -
+cd ./applications/market && mvn clean package && cd -
+cd ./applications/room && mvn clean package && cd -
+cd ./applications/user && mvn clean package && cd -
 
-cd ../card
-mvn clean package
 
-cd ../game
-mvn clean package
-
-cd ../market
-mvn clean package
-
-cd ../room
-mvn clean package
-
-cd ../user
-mvn clean package
-
-cd ../
 docker-compose up --build -d
-
-cd ../full-front
-docker-compose up --build -d 
-
