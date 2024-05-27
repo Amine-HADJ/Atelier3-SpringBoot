9250a444966045c802f8905146914b98fd853192
Amine-HADJ <mohammed.hadj-hamdri@cpe.fr>
2024-05-27 09:44:46 +0200

    userservice

--- Docker/applications/room/src/main/java/com/cardgame/Room/Room.java
+++ Docker/applications/room/src/main/java/com/cardgame/Room/Room.java
@@ -1,5 +1,40 @@
 package com.cardgame.Room;
 
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Room {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private List users;
+
+    public Room() {
+    }
+
+    public Room(List users) {
+        this.users = users;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public List getUsers() {
+        return users;
+    }
+
+    public void setUsers(List users) {
+        this.users = users;
+    }
 }
