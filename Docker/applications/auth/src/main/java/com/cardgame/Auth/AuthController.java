d489ec296709b22f924b31f4486850271da2878b
Amine-HADJ <mohammed.hadj-hamdri@cpe.fr>
2024-05-27 08:40:00 +0200

    auth service

--- Docker/applications/auth/src/main/java/com/cardgame/Auth/AuthController.java
+++ Docker/applications/auth/src/main/java/com/cardgame/Auth/AuthController.java
@@ -1,5 +1,31 @@
 package com.cardgame.Auth;
 
+import com.cardgame.Auth.LoginRequest;
+import com.cardgame.Auth.AuthService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AuthController {
 
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/login")
+    @CrossOrigin(origins = "*")
+    public ResponseEntity<String> login(@RequestBody LoginRequest req) {
+        Integer check = authService.checkLogin(req.getUsernameOrEmail(), req.getPassword());
+        if (check > -1) {
+            return ResponseEntity.ok(check.toString());
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+    }
 }
--- Docker/applications/auth/src/main/java/com/cardgame/Auth/AuthService.java
+++ Docker/applications/auth/src/main/java/com/cardgame/Auth/AuthService.java
@@ -1,5 +1,26 @@
 package com.cardgame.Auth;
 
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
 public class AuthService {
 
+    private final RestTemplate restTemplate;
+    private final String userServiceUrl = "http://localhost:8082";
+
+    public AuthService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public Integer checkLogin(String identifier, String password){
+        AppUser userByEmail = restTemplate.getForObject(userServiceUrl + "/users/email/" + identifier, AppUser.class);
+        AppUser userByUsername = restTemplate.getForObject(userServiceUrl + "/users/username/" + identifier, AppUser.class);
+        AppUser user = userByEmail == null ? userByUsername : userByEmail;
+        if(password.equals(user.getPassword())){
+            return user.getId();
+        }
+       
+    }
+    
 }
