c4bad88e7fcde648cd5f311161913a0c84805e23
Amine-HADJ <mohammed.hadj-hamdri@cpe.fr>
2024-05-27 11:18:43 +0200

    aaaa

--- Docker/full-front/js/home.js
+++ Docker/full-front/js/home.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", async () => {
     cards = await fetch("../json/cardMarket.json").then((response) => response.json());
 
-    await fetch("http://localhost:8080/generateCards", {
+    await fetch("http://card/generateCards", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
--- Docker/setup.sh
+++ Docker/setup.sh
@@ -2,12 +2,12 @@
 
 # This script will generate .jar for each application ('mvn clean package') and then run all the neccessary containers for the project.
 
-cd ./applications/auth && mvn clean package && cd -
-cd ./applications/card && mvn clean package && cd -
-cd ./applications/game && mvn clean package && cd -
-cd ./applications/market && mvn clean package && cd -
-cd ./applications/room && mvn clean package && cd -
-cd ./applications/user && mvn clean package && cd -
+cd ./applications/auth && mvn clean package && cd ..
+cd ./card && mvn clean package && cd ..
+cd ./game && mvn clean package && cd ..
+cd ./market && mvn clean package && cd ..
+cd ./room && mvn clean package && cd ..
+cd ./user && mvn clean package && cd ../..
 
 
 docker-compose up --build -d
