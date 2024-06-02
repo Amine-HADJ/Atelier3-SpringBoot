5ed7fb2b5fea5e43fc881e9f47f35d71b05cc119
dstrn <dstrn825@gmail.com>
2024-06-02 14:41:14 +0200

    Update index.js

--- Docker/full-front/index.js
+++ Docker/full-front/index.js
@@ -3,11 +3,11 @@ const path = require('path');
 
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.use(express.static(path.join(__dirname)));
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'home.html'));
 });
 
